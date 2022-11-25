40b9389620a499b3268723a97a4d38c0fd170dcf
Thibaud-M326 <thibaudmaitre@gmail.com>
2022-11-25 14:25:13 +0100

    hello

---
+++ index.js
@@ -0,0 +1,35 @@
+// document.getElementsByClassName("title");
+
+// document.querySelector(".title").style.fontSize = "5rem";
+
+// ------------------------------------------------------------
+
+
+
+
+
+// Affichage nom dans la console
+
+
+//creer une variable firstName
+let firstName;
+
+//initialiser la variable
+firstName = "Thibaud";
+
+//Afficher la variable
+console.log(firstName);
+
+//lui allouer une valeur differente
+firstName = "Louis"
+
+//afficher la variable
+console.log(firstName);
+
+//creer et initialiser une constant lastName
+const lastName = "Maitre";
+
+lastName = "Jesus";
+
+//Afficher la constante
+console.log(lastName);
